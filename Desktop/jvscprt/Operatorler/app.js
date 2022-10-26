80a9cc45baa1c2e0df2ec89145040c1b8ed0e84d
salih <askd@gmail.com@>
2022-10-26 16:46:41 +0200

    1b

---
+++ Desktop/jvscprt/Operatorler/app.js
@@ -0,0 +1,26 @@
+// Matematiksel islemler
+
+var x =10;
+var y=20;
+var z=30;
+
+console.log(x+y);
+console.log(x-y);
+console.log(y/x);
+console.log(x*y);
+console.log(x*(y+z));
+console.log(x*y+z);
+/*
+x++;
+x--;
+
+
+console.log(x);
+*/
+// Karsilastirma operatorleri
+
+var sonuc = x>y;
+console.log(sonuc);
+console.log(x<y);
+console.log(x>=z);
+console.log(x==z);
