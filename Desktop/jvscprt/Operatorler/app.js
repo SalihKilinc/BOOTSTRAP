1c702aa2c9ee60a81922b34b723da37503fdaf59
salih <askd@gmail.com@>
2022-10-29 21:13:22 +0200

    1w

--- Desktop/jvscprt/Operatorler/app.js
+++ Desktop/jvscprt/Operatorler/app.js
@@ -2,7 +2,7 @@
 
 var x =10;
 var y=20;
-var z=30;
+var z=10;
 
 console.log(x+y);
 console.log(x-y);
@@ -24,3 +24,13 @@ console.log(sonuc);
 console.log(x<y);
 console.log(x>=z);
 console.log(x==z);
+console.log(x=="10");
+console.log(x==="10");// uc tane esittir isareti gelirse intiger mi string mi ona bakar ve ona gore cevap verir
+console.log(x!=z);
+
+
+// Mantiksal operatorleri
+
+console.log((15>10) && (8<7)); // and isareti
+console.log((21>20) || (2<1)); // or isaretinde
+console.log(!(x>z));// degildir yaptik unlem ile
